dc378f0e29de899584bba8858d0d3cf8d829512a
adrievelyn <adrievelyn@gmail.com>
2020-01-13 15:45:12 -0300

    Updates

---
+++ precache-manifest.02a10a31b91739e6efe8f7fcb4037bde.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3bc6fb3a0060fc8c3e72cffb654c5e",
+    "url": "/adrievelyn/Portfolio/index.html"
+  },
+  {
+    "revision": "83a0d5b9e7b80472c82d",
+    "url": "/adrievelyn/Portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "2c29f06b042bbb259b71",
+    "url": "/adrievelyn/Portfolio/static/css/main.311ddf69.chunk.css"
+  },
+  {
+    "revision": "83a0d5b9e7b80472c82d",
+    "url": "/adrievelyn/Portfolio/static/js/2.66aed393.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/adrievelyn/Portfolio/static/js/2.66aed393.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c29f06b042bbb259b71",
+    "url": "/adrievelyn/Portfolio/static/js/main.3a5923db.chunk.js"
+  },
+  {
+    "revision": "ede8ee903fd1c9d2e8d2",
+    "url": "/adrievelyn/Portfolio/static/js/runtime-main.90a8b6bf.js"
+  },
+  {
+    "revision": "2e2d08a41019b6135d30d5a02330d162",
+    "url": "/adrievelyn/Portfolio/static/media/BurgerQueen.2e2d08a4.png"
+  },
+  {
+    "revision": "de281d62219f9e2392119b9754ea47f6",
+    "url": "/adrievelyn/Portfolio/static/media/RedSocial.de281d62.jpg"
+  },
+  {
+    "revision": "0d93f21d494ffe3a988c1f7e5ec92eeb",
+    "url": "/adrievelyn/Portfolio/static/media/SocialData.0d93f21d.jpg"
+  },
+  {
+    "revision": "e18062ca5b27695e263982a8dfcde548",
+    "url": "/adrievelyn/Portfolio/static/media/adriana_foto.e18062ca.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adrievelyn/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
