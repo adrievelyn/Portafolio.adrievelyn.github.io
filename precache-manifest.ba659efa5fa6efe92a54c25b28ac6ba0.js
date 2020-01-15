1c48332fae0d91c30c085fb8320a321b4e601759
adrievelyn <adrievelyn@gmail.com>
2020-01-15 15:40:20 -0300

    Updates

---
+++ precache-manifest.ba659efa5fa6efe92a54c25b28ac6ba0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312e2a6ee24df437dc971bed59727122",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c94f26a1171b611ddf2f",
+    "url": "/Portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a1678e2c5223f2df33c9",
+    "url": "/Portfolio/static/css/main.4a453138.chunk.css"
+  },
+  {
+    "revision": "c94f26a1171b611ddf2f",
+    "url": "/Portfolio/static/js/2.886c62bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Portfolio/static/js/2.886c62bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1678e2c5223f2df33c9",
+    "url": "/Portfolio/static/js/main.5b61a004.chunk.js"
+  },
+  {
+    "revision": "8f71a7b0ced28a6e37d3",
+    "url": "/Portfolio/static/js/runtime-main.ba21c266.js"
+  },
+  {
+    "revision": "2e2d08a41019b6135d30d5a02330d162",
+    "url": "/Portfolio/static/media/BurgerQueen.2e2d08a4.png"
+  },
+  {
+    "revision": "d97f931a47434a4a1807c39cae36839a",
+    "url": "/Portfolio/static/media/CV.d97f931a.svg"
+  },
+  {
+    "revision": "de281d62219f9e2392119b9754ea47f6",
+    "url": "/Portfolio/static/media/RedSocial.de281d62.jpg"
+  },
+  {
+    "revision": "0d93f21d494ffe3a988c1f7e5ec92eeb",
+    "url": "/Portfolio/static/media/SocialData.0d93f21d.jpg"
+  },
+  {
+    "revision": "e18062ca5b27695e263982a8dfcde548",
+    "url": "/Portfolio/static/media/adriana_foto.e18062ca.png"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/Portfolio/static/media/github2.e4f2d6b0.svg"
+  },
+  {
+    "revision": "deda33aea2bfa9216df4b9abc2d17698",
+    "url": "/Portfolio/static/media/gmail.deda33ae.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/Portfolio/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "2a4875a9de274d1beb71d176c632e747",
+    "url": "/Portfolio/static/media/logo.2a4875a9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.ab9ee387453cb24ad611014601d09756.js"
+  "/Portfolio/precache-manifest.ba659efa5fa6efe92a54c25b28ac6ba0.js"
 );
 
 self.addEventListener('message', (event) => {
